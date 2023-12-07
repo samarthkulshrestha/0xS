7fac347d615663fc83aec16b75a95f120b40b461
samarth kulshrestha <samarthkulshrestha@protonmail.com>
2023-12-07 20:56:27 +0530

    informing the compiler about freestanding env

--- src/build.sh
+++ src/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 nasm -f elf32 kernel.asm -o ../target/kasm.o
-gcc -m32 -c kernel.c -o ../target/kc.o
+gcc -m32 -c kernel.c -o ../target/kc.o -ffreestanding
 ld -m elf_i386 -T link.ld -o ../target/kernel.bin ../target/kasm.o ../target/kc.o
 qemu-system-i386 -kernel ../target/kernel.bin
 cp ../target/kernel.bin ../0s/boot/
