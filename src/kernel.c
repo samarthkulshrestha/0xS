1494bc576130db1ac6e31bebf8c289469f260894
samarth kulshrestha <samarthkulshrestha@protonmail.com>
2023-12-07 21:47:55 +0530

    implemented basic prompt

--- src/kernel.c
+++ src/kernel.c
@@ -1,5 +1,25 @@
-kmain() {
-    char* vidmem = (char*)0xB8000;
-    vidmem[0] = 'X';
-    vidmem[1] = 0x02;
+#include "include/screen.h"
+#include "include/kb.h"
+#include "include/string.h"
+
+
+int kmain() {
+    clear_screen();
+    print("welcome to 0s!\n");
+
+    while(1) {
+        print("\nqc8x:  ");
+        string ch = read_str();
+
+        if (str_eql(ch, "cmd")) {
+            print("\nyou're already in cmd.\n");
+        }
+        else if (str_eql(ch, "clear")) {
+            clear_screen();
+        }
+        else {
+            print("\ncommand not found.\n");
+        }
+        print("\n");
+    }
 }
