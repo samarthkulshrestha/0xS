3fa1ed1013fa07377c4280bc0ba7b479c900b934
samarth kulshrestha <samarthkulshrestha@protonmail.com>
2023-12-07 20:58:26 +0530

    added str_eql(). compares strings

--- src/include/string.h
+++ src/include/string.h
@@ -10,4 +10,19 @@ uint16 strlength(string ch) {
     return --i;
 }
 
+uint8 str_eql(string ch1, string ch2) {
+    uint8 result = 1;
+    uint8 size = strlength(ch1);
+
+    if (size != strlength(ch2)) { result = 0; }
+    else {
+        uint8 i = 0;
+
+        for (i; i <= size; i++) {
+            if (ch1[i] != ch2[i]) { result = 0; }
+        }
+    }
+    return result;
+}
+
 #endif
